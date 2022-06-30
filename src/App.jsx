206a7f5e6335fba8f628878b6c1d6006991b8f36
hayitmurod707 <hayitmurod707@gmail.com>
2022-06-30 14:52:29 +0500

    installed react app with prop-types and styled-components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import PasswordInput from './PasswordInput';
+const StyledElement = styled.div`
+	align-items: center;
+	display: flex;
+	height: 100vh;
+	justify-content: center;
+	width: 100vw;
+`;
+const App = () => {
+	const [value, setValue] = useState('');
+	return (
+		<StyledElement>
+			<PasswordInput
+				onChange={e => setValue(e.target.value)}
+				value={value}
+			/>
+		</StyledElement>
+	);
+};
+export default App;
